415835c92aad2db5c7bdb25ca1586fc94eeaf532
kangazoom <michellelcronin@gmail.com>
2018-08-20 03:38:54 -0700

    Completed first pass of BinaryandDecimal exercise

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,15 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  if binary_array.length == 8
+    length = binary_array.length
+    decimal_num = 0
+
+    length.times do |index|
+      decimal_num += binary_array[(length-1)-index] * 2**(index)
+      index += 1
+    end
+
+    return decimal_num
+  end
 end
