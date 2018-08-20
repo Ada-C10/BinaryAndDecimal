b1967197e0665c7b30a31fbe873b5d250796ae00
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-08-19 18:54:20 -0700

    finish homework version1

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -4,6 +4,11 @@
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
+require 'pry'
+
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  # raise NotImplementedError
+   sum = binary_array[0] * (2 ** 7) + binary_array[1] *(2 ** 6) + binary_array[2] * (2 ** 5) + binary_array[3] * (2 ** 4) + binary_array[4] * (2 ** 3) + binary_array[5] * (2 ** 2) + binary_array[6] * (2 ** 1) + binary_array[7] * (2 ** 0)
+   return sum
+
 end
