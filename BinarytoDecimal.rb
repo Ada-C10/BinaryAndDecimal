1b317cad1411e9214fe718affc01a2e1e56bec92
tfrego <30635441+tfrego@users.noreply.github.com>
2018-08-20 19:21:36 -0700

    Trang - Binary to Decimal

---
+++ BinarytoDecimal.rb
@@ -0,0 +1,18 @@
+# A method named `binary_to_decimal` that receives as input an array of size 8.
+# The array is randomly filled with 0’s and 1’s.
+# The most significant bit is at index 0.
+# The least significant bit is at index 7.
+# Calculate  and return the decimal value for this binary number using
+# the algorithm you devised in class.
+def binary_to_decimal(binary_array)
+  index = 7
+  decimal_number = 0
+  binary_array.each do |i|
+    if i != 0 && i != 1
+      raise Not Implemented Error
+    end
+    decimal_number += i * (2 ** index)
+    index -= 1
+  end
+  return decimal_number
+end
