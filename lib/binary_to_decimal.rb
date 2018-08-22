c062d566c34a780fb03a17624c07b67bb21b591d
Melissa O'Hearn <melissa.ohearn@gmailcom>
2018-08-22 08:44:25 -0700

    added code block to change a binary number to a decimal

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -4,6 +4,11 @@
 # The least significant bit is at index 7.
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
-def binary_to_decimal(binary_array)
-  raise NotImplementedError
+# def binary_to_decimal(binary_array)
+#   raise NotImplementedError
+# end
+
+def binary_to_decimal(bin_array)
+  decimal = (bin_array[7] * 1) + (bin_array[6] * 2) + (bin_array[5] * 4) + (bin_array[4] * 8) + (bin_array[3] * 16) + (bin_array[2] * 32) + (bin_array[1] * 64) + (bin_array[0] * 128)
+return decimal
 end
